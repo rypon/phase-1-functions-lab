60f436246c34e77b4bc98392494a0dda8814499e
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-10 22:13:14 -0500

    commit tree 1c92ef63f7637d7f8c6f3c5e779ef7da6051041a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+const hqBlock = 42;
+
+function distanceFromHqInBlocks(curBlock) {
+    if (curBlock >= 42) {
+        return curBlock - hqBlock;
+    } else {
+        return hqBlock - curBlock;
+    }
+
+}
+distanceFromHqInBlocks(curBlock);
+
+
+function distanceFromHqInFeet(curBlock) {
+    distanceFromHqInBlocks(curBlock);
+    return distanceFromHqInBlocks(curBlock) * 264;
+}
+distanceFromHqInFeet(curBlock);
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    if (start > destination) {
+        return (start - destination) * 264;
+    } else {
+        return (destination - start) * 264;
+    }
+}
+distanceTravelledInFeet(start, destination);
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    distanceTravelledInFeet(start, destination);
+
+}
+calculatesFarePrice(start, destination);
